8528ff17ddb64a475b64dd2234196a6043559d2c
miguelsotobranding <miguelsotobranding@gmail.com>
2026-09-24 02:17:50 -0500

    Add files via upload

---
+++ Landing.jsx
@@ -0,0 +1,9 @@
+export default function Landing(){
+return <main className="landing">
+<h1>Cali Colecciona</h1>
+<h2>Descubre Cali pieza a pieza</h2>
+<p>Vive experiencias, lugares y recuerdos que puedes coleccionar.</p>
+<button>Comenzar experiencia</button>
+<button>Empresas</button>
+</main>
+}
